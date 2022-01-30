4b905508c4b075c6465dfa147bbcdce371d8c743
Fcb-rohit <rightrohit96@gmail.com>
2022-01-30 21:55:47 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ function App() {
             <div className="w3-display-container">
               <img src="rahul.jpg" style={{ width: "100%" }} alt="Avatar" />
               <div className="w3-display-bottomleft w3-container w3-text-black">
-                <h2 style={{ color: "#fff" }}>Rahul Netrapal</h2>
+                <h2 style={{ color: "#000" }}>Rahul Netrapal</h2>
               </div>
             </div>
             <div className="w3-container">
